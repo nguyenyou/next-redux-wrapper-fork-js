3bcdbbd56608d910f5847923df200b2e26535ddb
tunguyen <tunguyen@chotot.vn>
2022-02-13 13:30:40 +0700

    next

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,9 @@
+const { devices } = require('@playwright/test')
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   retries: 1,
-  workers: 3,
+  workers: 5,
   use: {
     headless: true,
     video: 'on-first-retry',
@@ -9,6 +11,34 @@ const config = {
     trace: 'on-first-retry',
     baseURL: 'http://localhost:3000',
   },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    // {
+    //   name: 'Pixel 4',
+    //   use: {
+    //     browserName: 'chromium',
+    //     ...devices['Pixel 4'],
+    //   },
+    // },
+    // {
+    //   name: 'iPhone 11',
+    //   use: {
+    //     browserName: 'webkit',
+    //     ...devices['iPhone 11'],
+    //   },
+    // },
+  ],
 }
 
 module.exports = config
